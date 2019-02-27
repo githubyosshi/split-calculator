2eb55ab27c47b0e8bcb6f639efd423a56015e907
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-02-27 19:55:29 +0900

    Formula calculation completed

--- js/main.js
+++ js/main.js
@@ -9,14 +9,22 @@
   var reset = document.getElementById('reset');
 
   btn.addEventListener('click', function() {
+    var payLess;
+    var short;
+    var payMore;
+    var over;
+    var str;            // 結果の文字列
     // A. 300 (payLess) ... 100 (shart) 不足
     // B. 400 (payMore) ... 200 (over) 余り
     // payLess = 1000 / 3;        // 333.333...
     // payLess = 1000 / 3 / 100;  // 3.33333...
-    payLess = Math.floor(1000 / 3 / 100) * 100;   // 300  小数点以下切捨て
-    short = 1000 - (300 * 3);     // 100不足
-    payMore = Math.ceil(1000 / 3 / 100) * 100;    // 400  小数点以下切上げ
-    over = Math.abs(1000 - (400 * 3));        // 200余り
-
+    payLess = Math.floor(price.value / num.value / unit.value) * unit.value;   // 300  小数点以下切捨て
+    short = price.value - (payLess * num.value);     // 100不足
+    payMore = Math.ceil(price.value / num.value / unit.value) * unit.value;    // 400  小数点以下切上げ
+    over = Math.abs(price.value - (payMore * num.value));        // 200余り
+    str =
+      '一人 ' + payLess + ' 円だと ' + short + ' 円足りません。' +
+      '一人 ' + payMore + ' 円だと ' + over + ' 円余ります。';
+    result.textContent = str;
   });
 })();
